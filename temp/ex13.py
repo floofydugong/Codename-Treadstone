25ad821b3574aa5786d84ad14498386193e4c789
Haywire <email.john.huynh@gmail.com>
2015-10-12 18:27:03 -0400

    Initial Commit for Exercises

---
+++ temp/ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+# argv stands for 'argument variable'
+
+script, arg1, arg2, arg3 = argv
+
+print "The script is caled:", script
+print "Your first variable is:", raw_input()
+print "Your second variable is:", arg2
+print "Your third variable is:", arg3
