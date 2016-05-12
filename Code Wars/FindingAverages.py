60427a95f15355d0f2e600db8ad56b618983ae51
Hayw1re <email.john.huynh@gmail.com>
2016-05-11 20:10:56 -0400

    Removing old Code Wars Folder

--- Code Wars/FindingAverages.py
+++
@@ -1,9 +0,0 @@
-def average(x):
-   if type(x) is str or not x:
-      print "Incorrect";
-   elif len(x) == 1:
-      print x[0]
-   else:
-      print sum(x)/len(x)
-
-average([305])
--- Code Wars/VariableAssignmentDebug.py
+++
@@ -1,7 +0,0 @@
-def VariableAssignmentDebug():
-   a = 'dev';  
-   b = 'Lab';
-   name = a+b
-   print name
-
-VariableAssignmentDebug();
