3b3c3166fcfd01294b2affd61bb780758fdb3d32
Hayw1re <email.john.huynh@gmail.com>
2016-11-21 01:25:22 -0800

    Adding IPv4 solution

--- CodeWars_Solutions/6_kyu/IPv4AddressCheck.py
+++ CodeWars_Solutions/6_kyu/IPv4AddressCheck.py
@@ -1,14 +1,22 @@
-import re
-
 def ipv4_address(address):
-    
-    split_ip = address.split()
 
-    if len(split_ip) < 4:
-        print("Fail")
-    #Check for leading Zeroes
+    split_ip = address.split(".")
+
+    if len(split_ip) != 4:
+        return(False)
 
     #Values are between 0-255
+    for i in split_ip:
+        if len(i) > 1 and i[0] == '0':
+            return(False)
+        try:
+            int_value = int(i)
+            if int_value >= 0 and int_value <= 255 and i.isalnum():
+                continue
+            else:
+                return(False)
+        except:
+            return(False)
+    return(True)
 
-
-ipv4_address('127.0.0.1')
+ipv4_address('127.0.0.1\n')
