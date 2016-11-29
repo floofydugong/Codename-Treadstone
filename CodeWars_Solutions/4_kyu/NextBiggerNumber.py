7017815c6320257757e01b9f433a9329a4b2873b
Hayw1re <email.john.huynh@gmail.com>
2016-11-29 03:06:08 -0800

    Adding non-factored solution to next_bigger

--- CodeWars_Solutions/4_kyu/NextBiggerNumber.py
+++ CodeWars_Solutions/4_kyu/NextBiggerNumber.py
@@ -1,31 +1,37 @@
-from itertools import permutations
-
 def next_bigger(n):
 
-    num_list = list(str(n))
+    num_list = str(n)
 
-    max_length = (len(num_list)) * -1
-    tail_cutoff = -2
-
-    if len(num_list) <= 2:
+    if len(num_list) < 3:
         reverse_num = int(''.join(num_list[::-1]))
-        return(reverse_num if reverse_num > n else -1)
+        return (reverse_num if reverse_num > n else -1)
+    else:
+        curr_idx = len(num_list) -1
 
-    while tail_cutoff >= max_length:
+        while curr_idx >= 1:
+            if num_list[curr_idx] > num_list[curr_idx-1]:
+                head_list = num_list[:curr_idx-1]
+                switch_list = [int(i) for i in num_list[curr_idx-1:]]
 
-        head_value = num_list[:tail_cutoff]
-        tail_values = num_list[tail_cutoff:]
+                val_to_beat= num_list[curr_idx-1]
 
-        tail_perms = list(permutations(tail_values))
+                set_list = sorted(list(set(switch_list)))
 
-        answer_list = [int(''.join(head_value)+''.join(i)) for i in tail_perms]
+                for val in set_list:
+                    if val > int(val_to_beat):
+                        new_start_val =  val
+                        break
 
-        greater_than_n = list(filter(lambda x: x > n, answer_list))
+                sort_switch_list = sorted(switch_list)
+                sort_switch_list.remove(new_start_val)
 
-        if greater_than_n and min(greater_than_n) > n:
-            return(min(greater_than_n))
-        else:
-            tail_cutoff-=1
-    return(-1)
+                sort_switch_list = ''.join(str(x) for x in sort_switch_list)
 
-next_bigger(144)
+                print(int(head_list+str(new_start_val)+sort_switch_list))
+                return(int(head_list+str(new_start_val)+sort_switch_list))
+            else:
+                curr_idx -= 1
+        return(-1)
+
+next_bigger(59884848459853)
+
