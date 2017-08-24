c0d80a74413eaf9a3832abd1117f2807b66ff710
Hayw1re <email.john.huynh@gmail.com>
2017-08-24 00:58:26 -0700

    Adding emirps exercise

---
+++ CodeWars_Solutions/5_kyu/Emirps.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/emirps/train/python
+
+def find_emirp(n):
+    # your code goes here
+
+    print (n)
+    return #[amount of emirps in the range(13, n + 1), largest emirp smaller than n, sum of all the emirps of this range.
+
+find_emirp(10)
